9a77fcd8e54ce5422a7f1a2cf0bbc1fd10a469fb
Dominik Urminský <urminsky.d@gmail.com>
2023-09-02 16:38:45 +0200

    updated branch endpoint

--- src/Branch.php
+++ src/Branch.php
@@ -26,7 +26,8 @@ final class Branch
             throw new \RuntimeException('API key can not be empty.');
         }
         $this->branchStorage = $branchStorage ?? new BranchStorageFile();
-        $this->jsonEndpoint = 'https://www.zasilkovna.cz/api/v3/' . $apiKey . '/branch.json';
+
+        $this->jsonEndpoint = 'https://pickup-point.api.packeta.com/v5/' . $apiKey . '/box/json';
     }
 
 
